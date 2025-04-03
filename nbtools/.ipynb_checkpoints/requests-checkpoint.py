730846f92881267dda3b70b4a4dc1a853a3eff6a
Max Winokan <mwinokan@me.com>
2025-04-03 13:58:30 +0000

    download UI

--- nbtools/.ipynb_checkpoints/requests-checkpoint.py
+++
@@ -1,8 +0,0 @@
-
-STACKS = { "staging":"https://fragalysis.xchem.diamond.ac.uk", "production": "https://fragalysis.diamond.ac.uk" }
-
-def target_list():
-    raise NotImplementedError
-
-def download():
-    raise NotImplementedError
--- nbtools/widgets.py
+++ nbtools/widgets.py
@@ -1,7 +1,8 @@
 
 import mrich
 import ipywidgets
-from .requests import STACKS, target_list
+from .requests import STACKS, target_list, download_target
+import re
 
 def download():
     _download_ui_1()
@@ -10,7 +11,7 @@ def download():
 def _download_ui_1():
     """Create widgets to request target list"""
     
-    mrich.h1("download target")
+    mrich.h3("download target")
     
     output = ipywidgets.Output()
     
@@ -28,19 +29,43 @@ def _download_ui_1():
     
     def button_func(button):
         with output:
-            
+            _download_ui_2(token=w_token.value, stack=w_stack.value)
     
     b_get_targets.on_click(button_func)
     
     display(w_stack, w_token, b_get_targets, output)
-
-
-def button_func(button):
-    with output:
-        print("Button was clicked")
-
-
     
-def _download_ui_2():
+    
+def _download_ui_2(stack: str, token: str | None = None):
     """widgets to request target download"""
-    raise NotImplementedError
+    
+    output = ipywidgets.Output()
+    
+    with mrich.loading("Getting targets..."):
+        targets = target_list(token=token, stack=stack)
+        
+    options = sorted((f"{d[1]} ({d[2]})" for d in targets))
+
+    w_target = ipywidgets.Dropdown(
+        options=options,
+        description='Target',
+    )
+    
+    w_destination = ipywidgets.Text(description="Destination", value=".")
+    
+    b_download = ipywidgets.Button(
+        description='Download',
+    )
+    
+    def button_func(button):
+        with output:
+            match = re.search(r"^(.*) \((lb[0-9]*-[0-9]*)\)$", w_target.value)
+            
+            name, tas = match.groups()
+
+            download_target(name=name, tas=tas, stack=stack, token=token, destination=w_destination.value)
+    
+    b_download.on_click(button_func)
+    
+    display(w_target, w_destination, b_download, output)
+    
